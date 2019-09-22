1792666654d3f5430fb05095c2cd26f810eea392
Michał <michu214@gmail.com>
2019-09-22 11:43:30 +0200

    utworzona klasa z webdriverem - otwiera firefoxa, nie odpala bookinga

--- src/main/java/bookingTest.java
+++ src/main/java/bookingTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,6 +10,8 @@ public class bookingTest {
         driver = new FirefoxDriver();
 
         driver.get("https://www.booking.com/index.pl.html");
-        driver.close();
+        driver.findElement(By.cssSelector("input.sb-searchbox__input")).sendKeys("Warszawa, mazowieckie, Polska");
+        driver.findElement(By.cssSelector("button.sb-searchbox__button")).click();
+
     }
 }
