8abe9fab4e5e27905c86f0621f8fdfb24bbb74b2
Michał <michu214@gmail.com>
2019-09-22 12:55:16 +0200

    utworzona klasa z webdriverem - otwiera firefoxa, nie odpala bookinga

--- src/main/java/bookingTest.java
+++ src/main/java/bookingTest.java
@@ -1,16 +1,22 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class bookingTest {
-    public static void main(String[] args){
-        WebDriver driver;
-        System.setProperty("webdriver.gecko.driver","src\\webdriver\\geckodriver.exe");
-        driver = new FirefoxDriver();
+import java.util.List;
 
+public class bookingTest {
+    public static void main(String[] args) {
+        WebDriver driver;
+        System.setProperty("webdriver.gecko.driver", "src\\webdriver\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
         driver.get("https://www.booking.com/index.pl.html");
         driver.findElement(By.cssSelector("input.sb-searchbox__input")).sendKeys("Warszawa, mazowieckie, Polska");
+        driver.findElement(By.cssSelector("button.sb-date-field__icon-btn")).click();
+        List<WebElement> dateIn = driver.findElements(By.cssSelector("td.bui-calendar__date"));
+
         driver.findElement(By.cssSelector("button.sb-searchbox__button")).click();
 
     }
